41cee6b0e6fea8f4c7e0665fa8a89ea311b3a3c1
flyer5200 <flyer5200@gmail.com>
2015-05-08 18:11:02 +0800

    add update kernel method

--- init-centos.sh
+++ init-centos.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 yum -y install wget
 
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
@@ -12,3 +14,20 @@ yum -y install lrzsz
 yum -y install net-tools
 
 hostnamectl set-hostname "minion1"
+
+
+#update kernel to latest stable kernel
+
+#import key
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+
+#switch rpm repo
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+
+#install latest stable kernel
+yum --enablerepo=elrepo-kernel install  kernel-ml-devel kernel-ml -y
+
+#set grub boot
+grub2-set-default 0
+
+reboot
