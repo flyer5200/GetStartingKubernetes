39818079cb8be1e8fbb949a32fca48eec9dcab33
flyer5200@gmail.com <flyer5200@gmail.com>
2015-05-11 15:57:13 +0800

    remove replace repo config

--- minion/install-docker.sh
+++ minion/install-docker.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 
-# replace default repo and download latest docker 1.6
-
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-
-
-cat <<EOF>> /etc/yum.repos.d/CentOS-Base.repo
-
-#add a latest repo
-[virt7-testing]
-name=virt7-testing
-baseurl=http://cbs.centos.org/repos/virt7-testing/x86_64/os/
-enabled=1
-gpgcheck=0
-exclude=kernel
-
-EOF
-
-
-
 yum -y install docker
 
 sudo sed -i 's|OPTIONS=|OPTIONS=--registry-mirror=http://95728259.m.daocloud.io |g' /etc/sysconfig/docker
