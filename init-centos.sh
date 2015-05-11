e1221dcf8644cbf32730b02a0595e030df5a4350
flyer5200 <flyer5200@gmail.com>
2015-05-11 13:20:48 +0800

    add latest repo(testing) config

--- init-centos.sh
+++ init-centos.sh
@@ -2,20 +2,49 @@
 
 yum -y install wget
 
+
+# replace default repo
+
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+
+cat <<EOF>> /etc/yum.repos.d/CentOS-Base.repo
+
+#add a latest repo
+[virt7-testing]
+name=virt7-testing
+baseurl=http://cbs.centos.org/repos/virt7-testing/x86_64/os/
+enabled=1
+gpgcheck=0
+exclude=kernel
+
+EOF
+
 
 yum clean all
 
 yum makecache
 
+
+# install base tools
+
 yum -y install lrzsz
 yum -y install net-tools
 
+
+# set hostname
+
 hostnamectl set-hostname "centos7"
 
+
+#stop firewalld and replace to iptables
+
 sh replace-firewall.sh
+
+
+
 
 #update kernel to latest stable kernel
 
